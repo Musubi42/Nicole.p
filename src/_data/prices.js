0095d3b35ad143b7de59167a2d7f722bfe26260d
Musubi42 <raphael.dev@protonmail.com>
2023-08-14 10:43:07 +0200

    Init Eleventy app

---
+++ src/_data/prices.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const environment = process.env.CONTEXT
+const api_key = environment !== "production"
+  ? process.env.STRIPE_TEST_KEY
+  : process.env.STRIPE_SECRET_KEY;
+const stripe = require("stripe")(api_key);
+
+// One product can have multiple prices but for my use case, it's 1:1 so this works without having to massage the data
+// TODO: add Expanding data docs link
+async function getPrices() {
+  const response = await stripe.prices.list({
+    expand: ["data.product"],
+  });
+  return response.data.filter(price => price.active);
+}
+
+module.exports = async function () {
+  return await getPrices();
+};
